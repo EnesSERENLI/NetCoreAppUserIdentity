a239aad85b253f1f606a164431ccc76a00e09ceb
EnesSERENLI <enes.serenli@hotmail.com>
2022-06-14 19:21:37 +0300

    MVC is included in services. Added to Pipeline. MVC structures defined.

--- NetCoreAppUserIdentity/Startup.cs
+++ NetCoreAppUserIdentity/Startup.cs
@@ -16,6 +16,8 @@ namespace NetCoreAppUserIdentity
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //MVC Service
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +30,8 @@ namespace NetCoreAppUserIdentity
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseEndpoints(x => {
+                x.MapControllerRoute(name: "default", pattern: "{Controller=Home}/{Action=Index}/{id?}");
             });
         }
     }
