cb0d84f5219297fe788b2a06da65169e3036f020
EnesSERENLI <enes.serenli@hotmail.com>
2022-06-14 20:13:31 +0300

    Layout page created. Taghelpers were added with Viewimport and necessary paths were defined.

--- NetCoreAppUserIdentity/Controllers/HomeController.cs
+++ NetCoreAppUserIdentity/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreAppUserIdentity.Models.Entity;
 
 namespace NetCoreAppUserIdentity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
--- NetCoreAppUserIdentity/Startup.cs
+++ NetCoreAppUserIdentity/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NetCoreAppUserIdentity.Models.Context;
+using NetCoreAppUserIdentity.Models.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,8 +22,17 @@ namespace NetCoreAppUserIdentity
             services.AddControllersWithViews();
             //Context
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer("server=DESKTOP-JOE5KI8\\SQLEXPRESS02;database=AppUserIdentityDb;Trusted_Connection=True;"));
+            //AppUser
+            services.AddIdentity<AppUser,IdentityRole>(x=>
+            {
+                //Þifre yapýlandýrmasý
+                x.Password.RequireDigit = false; //Sayý içersinmi (farketmez..)
+                x.Password.RequireLowercase = false; //Küçük harf zorunluluðu
+                x.Password.RequireUppercase = false; //Büyük harf zorunluluðu
+                x.Password.RequireNonAlphanumeric = false; //Özel karakter içersinmi
+                x.Password.RequiredLength = 6; //Min 6 karakter..
+            }).AddEntityFrameworkStores<AppDbContext>();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -29,10 +40,8 @@ namespace NetCoreAppUserIdentity
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
             app.UseStaticFiles();
-
             app.UseEndpoints(x => {
                 x.MapControllerRoute(name: "default", pattern: "{Controller=Home}/{Action=Index}/{id?}");
             });
