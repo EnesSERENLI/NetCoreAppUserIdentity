241c2cf91ac2e8d598308ef12222205df366fd13
EnesSERENLI <enes.serenli@hotmail.com>
2022-06-14 21:28:47 +0300

    UserController created. Users are listed.

---
+++ NetCoreAppUserIdentity/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreAppUserIdentity.Models.Entity;
+using System.Linq;
+
+namespace NetCoreAppUserIdentity.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public UserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public UserManager<AppUser> UserManager { get; }
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
